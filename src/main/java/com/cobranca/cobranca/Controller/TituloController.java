e59d95963c0e1a991aad98fc2708afd1bc7c0509
brunocesar <brunocesar.oc@hotmail.com>
2018-03-04 11:44:19 -0300

    - Retornando um ModelAndView com um objeto todosStatusTitulo para ser iterado dentro da view - Setando "todosStatusTitulo" para qualquer view retornada

--- src/main/java/com/cobranca/cobranca/Controller/TituloController.java
+++ src/main/java/com/cobranca/cobranca/Controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.cobranca.cobranca.Controller;
 
 import com.cobranca.cobranca.Dao.ITitulo;
+import com.cobranca.cobranca.Model.StatusTitulo;
 import com.cobranca.cobranca.Model.Titulo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -16,8 +21,11 @@ public class TituloController {
     private ITitulo iTitulo;
 
     @RequestMapping("/novo")
-    public String novo() {
-        return "CadastroTitulo"; // sem ".html" , para mudancas futuras , ex: JSP
+    public ModelAndView novo() {
+
+        ModelAndView mv = new ModelAndView("CadastroTitulo");// sem ".html" , para mudancas futuras , ex: JSP
+        mv.addObject("todosStatusTitulo", StatusTitulo.values()); //Retornar todos os status para iterar dinamicamente na view
+        return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -29,6 +37,11 @@ public class TituloController {
         modelAndView.addObject("mensagem" , "Título salvo com sucesso!");
 
         return modelAndView;
+    }
+
+    @ModelAttribute("todosStatusTitulo") // Atributos vai estar disponível em todas as views sem precisar replicar código
+    public List<StatusTitulo> todosStatusTitulo(){
+        return Arrays.asList(StatusTitulo.values());
     }
 
 }
