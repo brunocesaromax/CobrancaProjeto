a4510473eda4a7fc620721fba722e5216035e46e
brunocesar <brunocesar.oc@hotmail.com>
2018-03-07 12:56:28 -0300

    - Implementação da exclusão de um título com modal para confirmação de exclusão feita com js - Separação entre mensagens Geral e mensagens de erro no sistema - Modal de dialogo de exlucao feito com bootstrap e trabalhado no js

--- src/main/java/com/cobranca/cobranca/Controller/TituloController.java
+++ src/main/java/com/cobranca/cobranca/Controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,13 +21,15 @@ import java.util.List;
 @RequestMapping("/titulos")
 public class TituloController {
 
+    public static final String CADASTRO_VIEW = "CadastroTitulo";
+
     @Autowired
     private ITitulo iTitulo;
 
     @RequestMapping("/novo")
     public ModelAndView novo() {
 
-        ModelAndView mv = new ModelAndView("CadastroTitulo");// sem ".html" , para mudancas futuras , ex: JSP
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);// sem ".html" , para mudancas futuras , ex: JSP
         mv.addObject(new Titulo());
         return mv;
     }
@@ -35,7 +38,7 @@ public class TituloController {
     public String salvar(@Validated Titulo titulo, Errors errors , RedirectAttributes attributes) { //Spring transforma o corpo que vem para o servidor no objeto esperado , gracas ao mesmo nome de atributos na view
 
         if(errors.hasErrors()){ // Validando o objeto titulo se apresentar erro manda para a página de cadastro
-            return "CadastroTitulo";
+            return CADASTRO_VIEW;
         }
 
         iTitulo.save(titulo);
@@ -51,6 +54,28 @@ public class TituloController {
         ModelAndView mv = new ModelAndView("PesquisaTitulos");
         mv.addObject("titulos",titulos);
         return mv;
+    }
+
+    // Por causa da aplicação estar usando JPA Repository,
+    // o próprio spring consegue relacionar o id a um objeto titulo específico, não necessitando de buscar esse objeto no banco
+    @RequestMapping("{id}")
+    public ModelAndView edicao(@PathVariable("id") Titulo titulo){
+
+        //Titulo tituloBD = iTitulo.findOne(id);
+        ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+        mv.addObject(titulo);
+
+        return mv;
+    }
+
+    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
+    public String excluir(@PathVariable Long id , RedirectAttributes attributes){
+
+        iTitulo.delete(id);
+        attributes.addFlashAttribute("mensagem","Título excluído com sucesso!");
+
+        return "redirect:/titulos";
+
     }
 
     @ModelAttribute("todosStatusTitulo") // Atributos vai estar disponível em todas as views sem precisar replicar código
---
+++ src/main/resources/static/js/cobranca.js
@@ -0,0 +1,24 @@
+$('#confirmacaoExclusaoModal').on('show.bs.modal',function (event) {
+
+    var button = $(event.relatedTarget); //pega o botao que dispara o evento
+
+    var id = button.data('codigo');
+    var descricao = button.data('descricao');
+
+    var modal = $(this);
+    var form = modal.find('form');
+    var action = form.attr('action');
+
+    if (!strEndsWith(action,'/')){// se nao terminar com '/' concatena com a mesma
+        action += '/';
+    }
+
+    form.attr('action',action + id);// Alterando o link com o codigo do titulo
+
+    modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricao + '<strong>?');
+
+});//Seguindo o modal de exemplo do Bootstrap
+
+function strEndsWith(str, suffix) {
+    return str.match(suffix+"$")==suffix;
+}
