642750976ee2987aff54f19cc07de567e90ff7f8
brunocesar <brunocesar.oc@hotmail.com>
2018-03-08 12:26:52 -0300

    - Add arquivos js e css para utilizar datepicker do bootstrap - Acréscimo da opção de escolher data pelo calendário da própria aplicação - Validação com try catch no back-end da data de vencimento ao salvar titulo

--- src/main/java/com/cobranca/cobranca/Controller/TituloController.java
+++ src/main/java/com/cobranca/cobranca/Controller/TituloController.java
@@ -4,6 +4,7 @@ import com.cobranca.cobranca.Dao.ITitulo;
 import com.cobranca.cobranca.Model.StatusTitulo;
 import com.cobranca.cobranca.Model.Titulo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -35,31 +36,36 @@ public class TituloController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(@Validated Titulo titulo, Errors errors , RedirectAttributes attributes) { //Spring transforma o corpo que vem para o servidor no objeto esperado , gracas ao mesmo nome de atributos na view
+    public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) { //Spring transforma o corpo que vem para o servidor no objeto esperado , gracas ao mesmo nome de atributos na view
 
-        if(errors.hasErrors()){ // Validando o objeto titulo se apresentar erro manda para a página de cadastro
+        if (errors.hasErrors()) { // Validando o objeto titulo se apresentar erro manda para a página de cadastro
             return CADASTRO_VIEW;
         }
 
-        iTitulo.save(titulo);
-        attributes.addFlashAttribute("mensagem" , "Título salvo com sucesso!");
+        try {
+            iTitulo.save(titulo);
+            attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+            return "redirect:/titulos/novo"; // redirect é uma nova requisição
 
-        return "redirect:/titulos/novo"; // redirect é uma nova requisição
+        } catch (DataIntegrityViolationException e) { // Capturar erro quando data informada mesmo com o calendário for inválida, e retornar erro para o usuário
+            errors.rejectValue("dataVencimento", null, "Formato de data inválido");
+            return CADASTRO_VIEW;
+        }
     }
 
     @RequestMapping
-    public ModelAndView pesquisar(){
+    public ModelAndView pesquisar() {
 
         List<Titulo> titulos = iTitulo.findAll(); //todo: trocar posteriormente
         ModelAndView mv = new ModelAndView("PesquisaTitulos");
-        mv.addObject("titulos",titulos);
+        mv.addObject("titulos", titulos);
         return mv;
     }
 
     // Por causa da aplicação estar usando JPA Repository,
     // o próprio spring consegue relacionar o id a um objeto titulo específico, não necessitando de buscar esse objeto no banco
     @RequestMapping("{id}")
-    public ModelAndView edicao(@PathVariable("id") Titulo titulo){
+    public ModelAndView edicao(@PathVariable("id") Titulo titulo) {
 
         //Titulo tituloBD = iTitulo.findOne(id);
         ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
@@ -68,18 +74,19 @@ public class TituloController {
         return mv;
     }
 
-    @RequestMapping(value = "{id}",method = RequestMethod.DELETE)
-    public String excluir(@PathVariable Long id , RedirectAttributes attributes){
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public String excluir(@PathVariable Long id, RedirectAttributes attributes) {
 
         iTitulo.delete(id);
-        attributes.addFlashAttribute("mensagem","Título excluído com sucesso!");
+        attributes.addFlashAttribute("mensagem", "Título excluído com sucesso!");
 
         return "redirect:/titulos";
 
     }
 
-    @ModelAttribute("todosStatusTitulo") // Atributos vai estar disponível em todas as views sem precisar replicar código
-    public List<StatusTitulo> todosStatusTitulo(){
+    @ModelAttribute("todosStatusTitulo")
+    // Atributos vai estar disponível em todas as views sem precisar replicar código
+    public List<StatusTitulo> todosStatusTitulo() {
         return Arrays.asList(StatusTitulo.values());
     }
 
