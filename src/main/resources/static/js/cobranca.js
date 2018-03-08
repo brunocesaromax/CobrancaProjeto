692544ca57e38184fbef3363b97c21dcbb7fc198
brunocesar <brunocesar.oc@hotmail.com>
2018-03-08 11:09:16 -0300

    - Utilizando recurso tootip do bootstrap com js para aparecer referência em cima do excluir e do editar

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,4 +1,4 @@
-$('#confirmacaoExclusaoModal').on('show.bs.modal',function (event) {
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function (event) {
 
     var button = $(event.relatedTarget); //pega o botao que dispara o evento
 
@@ -9,16 +9,20 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal',function (event) {
     var form = modal.find('form');
     var action = form.data('url-base'); //Mudança de action para data 'url-base' para não duplicar o id na chamada do excluir
 
-    if (!strEndsWith(action,'/')){// se nao terminar com '/' concatena com a mesma
+    if (!strEndsWith(action, '/')) {// se nao terminar com '/' concatena com a mesma
         action += '/';
     }
 
-    form.attr('action',action + id);// Alterando o link com o codigo do titulo
+    form.attr('action', action + id);// Alterando o link com o codigo do titulo
 
     modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricao + '<strong>?');
 
 });//Seguindo o modal de exemplo do Bootstrap
 
 function strEndsWith(str, suffix) {
-    return str.match(suffix+"$")==suffix;
+    return str.match(suffix + "$") == suffix;
 }
+
+$(function () { // Usando Jquery para aparecer descrição dos botões de excluir e editar
+    $('[rel="tooltip"]').tooltip();
+});
