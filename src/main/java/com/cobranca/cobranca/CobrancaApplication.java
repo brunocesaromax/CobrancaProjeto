117b51714248e7eca8073f02a0283db6f42113f9
brunocesar <brunocesar.oc@hotmail.com>
2018-03-09 19:51:38 -0300

    - Deixando o sistema anda mais responsivo com CSS e algumas metas - Deixando o pom.xml para gerar um .war com o nome do projeto artifactId - Colocando o sistema todo com o @{} do tymeleaf para reconhecer automaticamente o context path onde a aplicação estará rodando

--- src/main/java/com/cobranca/cobranca/CobrancaApplication.java
+++ src/main/java/com/cobranca/cobranca/CobrancaApplication.java
@@ -3,7 +3,10 @@ package com.cobranca.cobranca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import java.util.Locale;
@@ -18,5 +21,15 @@ public class CobrancaApplication {
 	@Bean// Locale fixo do Brasil setado diretamente da configuração do spring
 	public LocaleResolver localeResolver(){
 		return new FixedLocaleResolver(new Locale("pt","BR"));
+	}
+
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+
+		/*Se o usuário digitar apenas a '/' sera redirecionado direto para '/titulos' */
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry){
+			registry.addRedirectViewController("/","/titulos");
+		}
 	}
 }
